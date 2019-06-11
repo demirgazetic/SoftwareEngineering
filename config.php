d8fc6d924bf399f61bcf55369a44de88ccee6f0e
Demir Gazetic <demirgazetic@hotmail.com>
2019-06-11 19:46:34 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,29 +1,13 @@
+<?php 
+	session_start();
+	// connect to database
+	$conn = mysqli_connect("ol5tz0yvwp930510.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "ybkfqgepc6hqneq4", "lsfhkpboog9go3ro", "sxl0a8ry4y96pg9z");
 
-<?php
-
-class Database 
-{
-    public $handler;
-
-    public function __construct()
-    {
-        $host = 'ol5tz0yvwp930510.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
-        $db   = 'sxl0a8ry4y96pg9z';
-        $user = 'ybkfqgepc6hqneq4';
-        $pass = 'lsfhkpboog9go3ro';
-        $dsn = "mysql:host=$host;port=3306;dbname=$db;";
-        
-        
-
-        $opt = [
-            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_EMULATE_PREPARES   => false,
-        ];
-
-        $this->handler = new PDO($dsn, $user, $pass, $opt);        
-    }
-}
-
+	if (!$conn) {
+		die("Error connecting to database: " . mysqli_connect_error());
+	}
+    // define base url
+    
+	define('ROOT_PATH', _DIR_);
+	define('BASE_URL', 'http://localhost:8080/');
 ?>
-
