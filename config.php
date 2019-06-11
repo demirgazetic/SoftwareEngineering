8d3fa20f1f6dfbf0dba72ba4c54aafdda95d4051
Semon <vedad_njuhovic@hotmail.com>
2019-06-11 19:00:55 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,26 +1,29 @@
+
 <?php
-// used to get mysql database connection
-class Database{
- 
-    // specify your own database credentials
-    private $host = "ol5tz0yvwp930510.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306";
-    private $db_name = "sxl0a8ry4y96pg9z";
-    private $username = "ybkfqgepc6hqneq4";
-    private $password = "lsfhkpboog9go3ro";
-    public $conn;
- 
-    // get the database connection
-    public function getConnection(){
- 
-        $this->conn = null;
- 
-        try{
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
-        }catch(PDOException $exception){
-            echo "Connection error: " . $exception->getMessage();
-        }
- 
-        return $this->conn;
+
+class Database 
+{
+    public $handler;
+
+    public function __construct()
+    {
+        $host = 'ol5tz0yvwp930510.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+        $db   = 'sxl0a8ry4y96pg9z';
+        $user = 'ybkfqgepc6hqneq4';
+        $pass = 'lsfhkpboog9go3ro';
+        $dsn = "mysql:host=$host;port=3306;dbname=$db;";
+        
+        
+
+        $opt = [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES   => false,
+        ];
+
+        $this->handler = new PDO($dsn, $user, $pass, $opt);        
     }
 }
+
 ?>
+
