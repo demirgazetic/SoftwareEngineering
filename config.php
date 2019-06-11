ebca5629bc0552237e79e7ed6d7027cb64008e07
Demir Gazetic <demirgazetic@hotmail.com>
2019-06-11 18:53:16 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,18 +1,26 @@
-
-
 <?php
-
-$link = mysqli_connect("ol5tz0yvwp930510.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "ybkfqgepc6hqneq4", "lsfhkpboog9go3ro", "sxl0a8ry4y96pg9z", 3306);
-
-if (!$link) {
-    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-    exit;
+// used to get mysql database connection
+class Database{
+ 
+    // specify your own database credentials
+    private $host = "ol5tz0yvwp930510.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306";
+    private $db_name = "sxl0a8ry4y96pg9z";
+    private $username = "ybkfqgepc6hqneq4";
+    private $password = "lsfhkpboog9go3ro";
+    public $conn;
+ 
+    // get the database connection
+    public function getConnection(){
+ 
+        $this->conn = null;
+ 
+        try{
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+        }catch(PDOException $exception){
+            echo "Connection error: " . $exception->getMessage();
+        }
+ 
+        return $this->conn;
+    }
 }
-
-echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
-echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
-
-mysqli_close($link);
 ?>
