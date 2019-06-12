32c92bc60eabd03b26d247682359fd6488793a95
Demir Gazetic <demirgazetic@hotmail.com>
2019-06-13 00:06:38 +0200

    Update navbar.php

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,4 +1,105 @@
-    <div class="navbar">
+
+<style>
+
+
+@media screen and (min-width: 769px) and (max-width: 1023px) {
+	.navbar {
+    display: flex;
+    padding: 1rem;
+    margin: 0 auto;
+    overflow: hidden;
+    background-color: #1a2451;
+    border-radius: 0px 0px 6px 6px;
+	}
+	.navbar ul {
+     margin-left: 0rem; 
+    list-style-type: none;
+    float: right;
+    padding-top: 8px;
+}
+}
+@media screen and (min-width: 480px) and (max-width: 768px) {.navbar {
+    display: flex;
+    padding: 1rem;
+    margin: 0 auto;
+    overflow: hidden;
+    background-color: #1a2451;
+    border-radius: 0px 0px 6px 6px;
+	}
+	.navbar ul {
+     margin-left: 0rem; 
+    list-style-type: none;
+    float: right;
+    padding-top: 8px;
+}} 
+
+@media screen and (min-width: 320px) and (max-width: 479px) {.navbar {
+    display: flex;
+    padding: 1rem;
+    margin: 0 auto;
+    overflow: hidden;
+    background-color: #1a2451;
+    border-radius: 0px 0px 6px 6px;
+	}
+	.navbar ul {
+     margin-left: 0rem; 
+    list-style-type: none;
+    float: right;
+    padding-top: 8px;
+}}
+
+@media screen and (max-width: 319px) {.navbar {
+    display: flex;
+    padding: 1rem;
+    margin: 0 auto;
+    overflow: hidden;
+    background-color: #1a2451;
+    border-radius: 0px 0px 6px 6px;
+	}
+	.navbar ul {
+     margin-left: 0rem; 
+    list-style-type: none;
+    float: right;
+    padding-top: 8px;
+}}
+
+@media screen and (max-width: 768px) {.navbar {
+    display: flex;
+    padding: 1rem;
+    margin: 0 auto;
+    overflow: hidden;
+    background-color: #1a2451;
+    border-radius: 0px 0px 6px 6px;
+	}
+	.navbar ul {
+     margin-left: 0rem; 
+    list-style-type: none;
+    float: right;
+    padding-top: 8px;
+}}
+
+
+
+@media screen and (min-width: 1024px) and (max-width: 1215px) { { display: flex;
+    padding: 1rem;
+    margin: 0 auto;
+    overflow: hidden;
+    background-color: #1a2451;
+    border-radius: 0px 0px 6px 6px;
+	}
+	.navbar ul {
+     margin-left: 0rem; 
+    list-style-type: none;
+    float: right;
+    padding-top: 8px;
+}
+
+}
+
+
+</style>
+
+<div class="navbar">
 			<div class="logo_div">
 				<a href="index.php"><img class="logoImg" src="./static/images/logo.png" alt=""></a>
 			</div>
